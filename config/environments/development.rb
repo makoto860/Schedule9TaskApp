2e56e669e29f087ed301961326289578830f5d68
EC2 Default User <ec2-user@ip-172-31-12-203.ap-northeast-1.compute.internal>
2022-06-02 10:48:10 +0000

    git-development.rbの追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,3 +74,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
+Rails.application.configure do
+  config.hosts << "d01ecd1b340b42059662daec681bdc86.vfs.cloud9.ap-northeast-1.amazonaws.com"
+end
