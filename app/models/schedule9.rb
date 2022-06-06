94ed5b1cee38e0c6513d6deded2ef04160aa4482
makoto860 <93705485+makoto860@users.noreply.github.com>
2022-06-06 20:44:42 +0900

    Create schedule9.rb

---
+++ app/models/schedule9.rb
@@ -0,0 +1,17 @@
+class Schedule9 < ApplicationRecord
+  validates :title, presence: true
+  validates :first_date, presence: true
+  validates :final_date, presence: true
+  
+  validate :final_date_after_today
+  
+  def final_date_after_today
+    if final_date == nil
+      errors.add(:final_date,"は翌日以降の日にちを選択して") 
+    elsif final_date < Date.tomorrow
+      errors.add(:final_date,"は翌日以降の日にちを選択して") 
+    else
+      "新規登録完了処理"
+    end
+  end
+end
